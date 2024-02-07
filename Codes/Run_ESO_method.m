58fce94ad9306724b2321b4512d386a83d72d885
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2024-02-07 23:28:59 +0100

    Add files via upload

--- Codes/Run_ESO_method.m
+++ Codes/Run_ESO_method.m
@@ -6,13 +6,16 @@ rng('shuffle', 'twister')
 mkdir('Figure');
 mkdir('Topology');
 
-%****Thermal properties******************************
+%****Thermal properties**************************
 high_conductivity = 10;
 low_conductivity = 1;
 heat_sink_temperature = 300;
 x_step = 0.001;
 p_vol=1e5;
 filling_ratio=0.3;
+%****ESO parameters******************************
+rank_cells_to_exchange=5;%maximal rank allowed for exchange
+number_cells_allowed_to_move=1;%must be less than or equal to rank_cells_to_exchange
 starting_image='50x100.bmp';
 
 disp('Trying to restart from previous run if any...')
@@ -23,139 +26,118 @@ if not(last_valid_file==0)
     flag=0;
     disp('Previous run detected, loading last known topology...')
     restart_topology=imread(['Topology/',folder(last_valid_file).name]);
-    [~,largeur,~]=size(restart_topology);
-    condi_limites_1=[restart_topology(:,1:largeur/2,:),restart_topology(:,largeur,:)];
+    [~,width,~]=size(restart_topology);
+    initial_boundary_conditions=[restart_topology(:,1:width/2,:),restart_topology(:,width,:)];
 else
     flag=1;
     disp('No preceding run detected, starting from a random topology...')
-    condi_limites_1=imread(starting_image);
+    initial_boundary_conditions=imread(starting_image);
 end
 
-[hauteur,largeur,profondeur]=size(condi_limites_1);
-nombre_images = max([hauteur,largeur]);
-condi_limites = zeros(hauteur,largeur);
-automate=zeros(hauteur,largeur);
-condi_temp = zeros(hauteur,largeur);
-f_temp=zeros(nombre_images);
-f_iter=1:1:nombre_images;
+[height,width,profondeur]=size(initial_boundary_conditions);
+number_of_images = max([height,width]);
+boundary_conditions = zeros(height,width);
+history_map=zeros(height,width);
 
 %conversion of an image to boundary conditions
-pixels_blancs=0;
-pixels_noirs=0;
-for k = 1:1:hauteur
-    for l = 1:1:largeur
-        rouge = condi_limites_1(k,l,1);
-        vert = condi_limites_1(k,l,2);
-        bleu = condi_limites_1(k,l,3);
-        
-        if (rouge == 255) && (vert == 255) && (bleu == 255)
-            choix = low_conductivity;
-            pixels_blancs=pixels_blancs+1;
+non_conductive_cells=0;
+conductive_cells=0;
+for k = 1:1:height
+    for l = 1:1:width
+        red = initial_boundary_conditions(k,l,1);
+        green = initial_boundary_conditions(k,l,2);
+        blue = initial_boundary_conditions(k,l,3);
+        if (red == 255) && (green == 255) && (blue == 255)
+            pixel = low_conductivity;
+            non_conductive_cells=non_conductive_cells+1;
         end
-        if (rouge == 127) && (vert == 127) && (bleu == 127)
-            choix = -2;
+        if (red == 127) && (green == 127) && (blue == 127)
+            pixel = -2;
         end
-        if (rouge == 0) && (vert == 0) && (bleu == 255)
-            choix = -3;
+        if (red == 0) && (green == 0) && (blue == 255)
+            pixel = -3;
         end
-        if (rouge == 0) && (vert == 0) && (bleu == 0)
-            choix = high_conductivity;
-            pixels_noirs=pixels_noirs+1;
+        if (red == 0) && (green == 0) && (blue == 0)
+            pixel = high_conductivity;
+            conductive_cells=conductive_cells+1;
         end
-        
-        condi_limites(k, l) = choix;
-        
+        boundary_conditions(k, l) = pixel;
     end
 end
 
 if flag==1
-disp('Filling blank image with conductive pixels...')
-nombre_pixels_conducteurs=ceil(pixels_blancs*filling_ratio);
-condi_limites=init_image(condi_limites,nombre_pixels_conducteurs, low_conductivity, high_conductivity);
+    disp('Filling blank image with conductive pixels...')
+    number_conductive_cells=ceil(non_conductive_cells*filling_ratio);
+    boundary_conditions=init_image(boundary_conditions,number_conductive_cells, low_conductivity, high_conductivity);
 end
 
 disp('Starting the ESO algorithm...');
 
 %variable pre-allocation
-temp=ones(hauteur,largeur).*heat_sink_temperature;
-condu_tab=zeros(hauteur,largeur,4);
-new_pos_in=zeros(hauteur,largeur);
-new_pos_out=zeros(hauteur,largeur);
-new_pos2=zeros(hauteur,largeur);
-gradients=zeros(hauteur,largeur);
-note=zeros(hauteur,largeur);
-condi_limites_2=condi_limites_1;
+temp=ones(height,width).*heat_sink_temperature;
+boundary_output=initial_boundary_conditions;
 affichage=zeros(1,4);
-
 m=last_valid_file;
 u=0;
 figure('Position',[100 100 600 600]);
 
-while max(max(automate))<20
+while max(max(history_map))<30
     tic
     m=m+1;
+    disp(' ');
     disp(['---------Epoch: ',num2str(m),'---------']);
     disp('Applying ESO algorithm...');
-    [condi_limites,growth,etching] = fun_ESO_algorithm(condi_limites,high_conductivity,low_conductivity,heat_sink_temperature,x_step,p_vol);
-    [somme_entropie, entropie, border_variance,variance, moyenne_temp,t_max,temp,grad, variance_grad]=finite_temp_direct_sparse(high_conductivity,low_conductivity,heat_sink_temperature,x_step,p_vol,condi_limites);
-    t_max_sortie(m-last_valid_file)=t_max;
-    for k = 1:1:hauteur
-        for l = 1:1:largeur
-            
-            choix = condi_limites(k, l) ;
-            
-            if choix == low_conductivity
-                rouge = 255;
-                vert = 255;
-                bleu = 255;
+    [boundary_conditions,growth,etching] = fun_ESO_algorithm(boundary_conditions,high_conductivity,low_conductivity,heat_sink_temperature,x_step,p_vol,rank_cells_to_exchange,number_cells_allowed_to_move);
+    [somme_entropie, entropie, border_variance,variance, moyenne_temp,t_max,temp,grad, variance_grad]=finite_temp_direct_sparse(high_conductivity,low_conductivity,heat_sink_temperature,x_step,p_vol,boundary_conditions);
+    history_tmax(m-last_valid_file)=t_max;
+    
+    for k = 1:1:height
+        for l = 1:1:width
+            pixel = boundary_conditions(k, l) ;
+            if pixel == low_conductivity
+                red = 255;
+                green = 255;
+                blue = 255;
             end
-            
-            if choix == -2
-                rouge = 127;
-                vert = 127 ;
-                bleu = 127 ;
+            if pixel == -2
+                red = 127;
+                green = 127 ;
+                blue = 127 ;
             end
-            if choix == -3
-                rouge = 0;
-                vert = 0 ;
-                bleu = 255;
+            if pixel == -3
+                red = 0;
+                green = 0 ;
+                blue = 255;
             end
-            if choix == high_conductivity
-                rouge = 0 ;
-                vert = 0 ;
-                bleu = 0;
+            if pixel == high_conductivity
+                red = 0 ;
+                green = 0 ;
+                blue = 0;
             end
-            
-            condi_limites_2(k,l,1)=rouge;
-            condi_limites_2(k,l,2)=vert;
-            condi_limites_2(k,l,3)=bleu;
-            
+            boundary_output(k,l,1)=red;
+            boundary_output(k,l,2)=green;
+            boundary_output(k,l,3)=blue;
         end
     end
     
-    
-    condi_limites_2=uint8(condi_limites_2);
-    miroir=fliplr(condi_limites_2(1:hauteur,1:largeur-1,:));
-    miroir2=fliplr(miroir);
-    arbre=[miroir2,miroir];
+    boundary_output=uint8(boundary_output);
+    mirror=fliplr(boundary_output(1:height,1:width-1,:));
+    mirror2=fliplr(mirror);
+    arbre=[mirror2,mirror];
     figure(1)
     subplot(2,4,1:2);
     
-    
     if (m-last_valid_file)>2
-        for i=2:1:length(t_max_sortie)
-            residual(i-1)=(t_max_sortie(i)-t_max_sortie(i-1))/(t_max_sortie(2)-t_max_sortie(1));
+        for i=2:1:length(history_tmax)
+            residuals(i-1)=(history_tmax(i)-history_tmax(i-1))/(history_tmax(2)-history_tmax(1));
         end
-        
-        if (m-last_valid_file)<100; plot(log10(abs(residual)),'.r'); end
-        if (m-last_valid_file)>=100; plot(log10(abs(residual(end-98:end))),'.r'); end
-        
+        if (m-last_valid_file)<100; plot(log10(abs(residuals)),'.r'); end
+        if (m-last_valid_file)>=100; plot(log10(abs(residuals(end-98:end))),'.r'); end
     end
-    
-    title('log10 Residuals');
+    title('log10 residuals');
     
     subplot(2,4,3:4);
-    imagesc([miroir2,miroir]);
+    imagesc([mirror2,mirror]);
     title('Topology');
     
     subplot(2,4,5);
@@ -173,13 +155,15 @@ while max(max(automate))<20
     title('Gradients');
     
     subplot(2,4,8);
-    automate(growth(1,1),growth(1,2))=automate(growth(1,1),growth(1,2))+1;
-    automate(etching(1,1),etching(1,2))=automate(etching(1,1),etching(1,2))+1;
-    imagesc(automate);
-    title('Redundancy');
+    for i=1:1:number_cells_allowed_to_move
+        history_map(growth(i,1),growth(i,2))=history_map(growth(i,1),growth(i,2))+1;
+        history_map(etching(i,1),etching(i,2))=history_map(etching(i,1),etching(i,2))+1;
+    end
+    imagesc(history_map);
+    title('History');
     
-    disp(['Maximal temperature: ',num2str(t_max_sortie(m-last_valid_file))]);
-    condi_limites_1=condi_limites_2;
+    disp(['Maximal temperature: ',num2str(history_tmax(m-last_valid_file))]);
+    initial_boundary_conditions=boundary_output;
     colormap jet
     pause(0.01);
     
@@ -187,6 +171,17 @@ while max(max(automate))<20
     imwrite(arbre,['Topology_kp_ko_',num2str(high_conductivity),'_phi_',num2str(filling_ratio),'.png']);
     saveas(gcf,['Figure/Figure_kp_ko_',num2str(high_conductivity),'_phi_',num2str(filling_ratio),'_',num2str(m,'%06.f'),'.png']);
     imwrite(arbre,['Topology/Topology_kp_ko_',num2str(high_conductivity),'_phi_',num2str(filling_ratio),'_',num2str(m,'%06.f'),'.png']);
+    
+    if (m-last_valid_file)>2
+        if residuals(end)<0
+            number_cells_allowed_to_move=number_cells_allowed_to_move-1;
+            if number_cells_allowed_to_move<1
+                number_cells_allowed_to_move=1;
+            end
+        end
+    end
+    disp(['Cells allowed to move: ',num2str(number_cells_allowed_to_move)]);
+    disp(['Max redunding moves: ',num2str(max(max(history_map)))]);
     toc
 end
 disp('Converged !');
--- Codes/fun_ESO_algorithm.m
+++ Codes/fun_ESO_algorithm.m
@@ -1,89 +1,100 @@
-function [condi_limites,growth,etching] = fun_ESO_algorithm(condi_limites,cond_haute,cond_basse,temp_puits,pas_x,p_vol)
-[hauteur,largeur,profondeur]=size(condi_limites);
-%******************************************Début de l'automate cellulaire**
-%identification des pixels à faire pousser
-rng('shuffle', 'twister')
-grow=zeros(hauteur,largeur);
-for k = 1:1:hauteur;
-    for l = 1:1:largeur;
-        if condi_limites(k,l)==cond_haute;
-            if condi_limites(k+1,l)==cond_basse; grow(k+1,l)=1; end;
-            if condi_limites(k-1,l)==cond_basse; grow(k-1,l)=1; end;
-            if condi_limites(k,l+1)==cond_basse; grow(k,l+1)=1; end;
-            if condi_limites(k,l-1)==cond_basse; grow(k,l-1)=1; end;
-        end;
-    end;
-end;
+function [boudary_conditions,growth,etching] = fun_ESO_algorithm(boudary_conditions,kp_k0,k0,heat_sink_temp,step_x,p_vol,rank_cells_to_exchange,number_cells_to_exchange)
+[height,width,~]=size(boudary_conditions);
+
+%Listing the cells to grow
+grow=zeros(height,width);
+for k = 1:1:height
+    for l = 1:1:width
+        if boudary_conditions(k,l)==kp_k0
+            if boudary_conditions(k+1,l)==k0; grow(k+1,l)=1; end
+            if boudary_conditions(k-1,l)==k0; grow(k-1,l)=1; end
+            if boudary_conditions(k,l+1)==k0; grow(k,l+1)=1; end
+            if boudary_conditions(k,l-1)==k0; grow(k,l-1)=1; end
+        end
+    end
+end
 
 m=0;
-for k = 1:1:hauteur;
-    for l = 1:1:largeur;
-        if grow(k,l)==1;
+for k = 1:1:height
+    for l = 1:1:width
+        if grow(k,l)==1
             m=m+1;
             grow_pos(m,1)=k;
             grow_pos(m,2)=l;
-        end;
-    end;
-end;
+        end
+    end
+end
 
-%******************************************Début de l'automate cellulaire**
-%identification des pixels à faire crever
-etch=zeros(hauteur,largeur);
-for k = 1:1:hauteur;
-    for l = 1:1:largeur;
-        if condi_limites(k,l)==cond_basse;
-            if condi_limites(k+1,l)==cond_haute; etch(k+1,l)=1; end;
-            if condi_limites(k-1,l)==cond_haute; etch(k-1,l)=1; end;
-            if condi_limites(k,l+1)==cond_haute; etch(k,l+1)=1; end;
-            if condi_limites(k,l-1)==cond_haute; etch(k,l-1)=1; end;
-        end;
-    end;
-end;
+%Listing the cells to kill
+etch=zeros(height,width);
+for k = 1:1:height
+    for l = 1:1:width
+        if boudary_conditions(k,l)==k0
+            if boudary_conditions(k+1,l)==kp_k0; etch(k+1,l)=1; end
+            if boudary_conditions(k-1,l)==kp_k0; etch(k-1,l)=1; end
+            if boudary_conditions(k,l+1)==kp_k0; etch(k,l+1)=1; end
+            if boudary_conditions(k,l-1)==kp_k0; etch(k,l-1)=1; end
+        end
+    end
+end
 
 m=0;
-for k = 1:1:hauteur;
-    for l = 1:1:largeur;
-        if etch(k,l)==1;
+for k = 1:1:height
+    for l = 1:1:width
+        if etch(k,l)==1
             m=m+1;
             etch_pos(m,1)=k;
             etch_pos(m,2)=l;
-        end;
-    end;
-end;
-
-
-
-%identification des pixels à faire pousser
-condi_limites_temp=zeros(hauteur,largeur,length(grow_pos));
-for m=1:1:length(grow_pos);
-    condi_temp=condi_limites;
-    condi_temp(grow_pos(m,1),grow_pos(m,2))=cond_haute;
-    condi_limites_temp(:,:,m)=condi_temp;
+        end
+    end
 end
-parfor m=1:1:length(grow_pos);
-    [somme_entropie, entropie, border_variance,variance, moyenne_temp,temp_max,temp,grad, variance_grad]=finite_temp_direct_sparse(cond_haute,cond_basse,temp_puits,pas_x,p_vol,condi_limites_temp(:,:,m));
+
+%identification of the cells to grow
+boudary_conditions_temp=zeros(height,width,length(grow_pos));
+for m=1:1:length(grow_pos)
+    condi_temp=boudary_conditions;
+    condi_temp(grow_pos(m,1),grow_pos(m,2))=kp_k0;
+    boudary_conditions_temp(:,:,m)=condi_temp;
+end
+parfor m=1:length(grow_pos)
+    % Variables output in this order :
+    % 1. Distance of the hotest cell to the heat sink (scalar)
+    % 2. Sum of cell entropy (scalar)
+    % 3. Entropy map (matrix)
+    % 4. Variance of temperatures accross the 1D adabatic borders (scalar)
+    % 5. Variance of temperatures accross the 2D domain (scalar)
+    % 6. Mean temperature (scalar)
+    % 7. Maximal temperature accross the 2D domain (scalar)
+    % 8. Map of temperatures (matrix)
+    % 9. map of thermal gradients (matrix)
+    % 10. Variance of gradients across the 2D domain (scalar)
+    [~,~,~,~,~,temp_max,~,~,~]=finite_temp_direct_sparse(kp_k0,k0,heat_sink_temp,step_x,p_vol,boudary_conditions_temp(:,:,m));
     grow_pos(m,3)=temp_max;
 end
 
 grow_pos=sortrows(grow_pos,3);
 
-%identification des pixels à faire crever
-condi_limites_temp=zeros(hauteur,largeur,length(etch_pos));
-for m=1:1:length(etch_pos);
-    condi_temp=condi_limites;
-    condi_temp(etch_pos(m,1),etch_pos(m,2))=cond_basse;
-    condi_limites_temp(:,:,m)=condi_temp;
+%identification of the cells to kill
+boudary_conditions_temp=zeros(height,width,length(etch_pos));
+for m=1:1:length(etch_pos)
+    condi_temp=boudary_conditions;
+    condi_temp(etch_pos(m,1),etch_pos(m,2))=k0;
+    boudary_conditions_temp(:,:,m)=condi_temp;
 end
-parfor m=1:1:length(etch_pos);
-    [somme_entropie, entropie, border_variance,variance, moyenne_temp,temp_max,temp,grad, variance_grad]=finite_temp_direct_sparse(cond_haute,cond_basse,temp_puits,pas_x,p_vol,condi_limites_temp(:,:,m));
+parfor m=1:length(etch_pos)
+    [~,~,~,~,~,temp_max,~,~,~]=finite_temp_direct_sparse(kp_k0,k0,heat_sink_temp,step_x,p_vol,boudary_conditions_temp(:,:,m));
     etch_pos(m,3)=temp_max;
 end
 etch_pos=sortrows(etch_pos,3);
 
-growth=grow_pos(ceil(5*rand),:);
-etching=etch_pos(ceil(5*rand),:);
+growth=grow_pos(1:rank_cells_to_exchange,1:2);%list allowed to grow
+etching=etch_pos(1:rank_cells_to_exchange,1:2);%list allowed to be killed
+order_growth=randperm(number_cells_to_exchange);%shuffle data for more randomness
+order_etch=randperm(number_cells_to_exchange);%shuffle data for more randomness
 
-%Echange des cellules
-condi_limites(etching(1,1),etching(1,2))=cond_basse;
-condi_limites(growth(1,1),growth(1,2))=cond_haute;
-%******************************************Fin de l'automate cellulaire****
+%Cell exchange
+for i=1:1:number_cells_to_exchange
+    boudary_conditions(etching(order_etch(i),1),etching(order_etch(i),2))=k0;
+    boudary_conditions(growth(order_growth(i),1),growth(order_growth,2))=kp_k0;
+end
+
